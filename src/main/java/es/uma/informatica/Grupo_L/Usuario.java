62fe4fbfa74739b357e5d466753b18493a63650a
AlvaroCaba <alvaro_caba2009@uma.es>
2021-03-31 09:09:26 -0700

    Union ramas

--- src/main/java/es/uma/informatica/Grupo_L/Usuario.java
+++ src/main/java/es/uma/informatica/Grupo_L/Usuario.java
@@ -12,7 +12,7 @@ import javax.persistence.*;
 @Entity
 
 public class Usuario implements Serializable {
-//hola esto funciona?
+//hola esto funciona?Andale che 
 	   
 	@Id @GeneratedValue
 	private Integer ID; //LONG
