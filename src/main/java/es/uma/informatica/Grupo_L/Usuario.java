fedaf6f2d2eacd685e767aa058591f29d6e17dbb
AlvaroCaba <alvaro_caba2009@uma.es>
2021-03-31 09:11:47 -0700

    Quitar andaleche

--- src/main/java/es/uma/informatica/Grupo_L/Usuario.java
+++ src/main/java/es/uma/informatica/Grupo_L/Usuario.java
@@ -12,7 +12,7 @@ import javax.persistence.*;
 @Entity
 
 public class Usuario implements Serializable {
-//hola esto funciona?Andale che 
+//hola esto funciona?
 	   
 	@Id @GeneratedValue
 	private Integer ID; //LONG
