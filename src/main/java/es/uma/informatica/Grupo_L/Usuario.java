702b8ce60f20e5031fe9b0d7bb252fde085f321b
AlvaroCaba <alvaro_caba2009@uma.es>
2021-03-31 09:38:03 -0700

    Usualio listo

--- src/main/java/es/uma/informatica/Grupo_L/Usuario.java
+++ src/main/java/es/uma/informatica/Grupo_L/Usuario.java
@@ -12,7 +12,7 @@ import javax.persistence.*;
 @Entity
 
 public class Usuario implements Serializable {
-//hola esto funciona?
+//hola esto funciona?Modificacion en rama usuario
 	   
 	@Id @GeneratedValue
 	private Integer ID; //LONG
