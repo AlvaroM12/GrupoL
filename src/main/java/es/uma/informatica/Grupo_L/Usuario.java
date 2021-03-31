fca690a6af3f5fcab7897aa24f46e1fb85b188f8
AlvaroCaba <alvaro_caba2009@uma.es>
2021-03-31 09:13:07 -0700

    PrimeraPrueba

--- src/main/java/es/uma/informatica/Grupo_L/Usuario.java
+++ src/main/java/es/uma/informatica/Grupo_L/Usuario.java
@@ -12,7 +12,7 @@ import javax.persistence.*;
 @Entity
 
 public class Usuario implements Serializable {
-//hola esto funciona?
+//hola esto funciona?Esta es la primera prueba
 	   
 	@Id @GeneratedValue
 	private Integer ID; //LONG
