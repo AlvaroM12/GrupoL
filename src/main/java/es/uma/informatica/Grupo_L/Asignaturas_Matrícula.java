a44e3c8260ae489f02eb0e6a0ec070717510b902
Marta Lopez <martalopezperez@uma.es>
2021-04-01 14:33:22 -0700

    Creación entidad Asignaturas_Matricula

---
+++ src/main/java/es/uma/informatica/Grupo_L/Asignaturas_Matrícula.java
@@ -0,0 +1,20 @@
+package es.uma.informatica.Grupo_L;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+/**
+ * Entity implementation class for Entity: Asignaturas_Matrícula
+ *
+ */
+@Entity
+
+public class Asignaturas_Matrícula implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	public Asignaturas_Matrícula() {
+		super();
+	}
+   
+}
