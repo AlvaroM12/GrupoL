1e8c4785213b2659885903e10656abdb8e493f4e
AlvaroM <alvaromm@uma.es>
2021-05-17 15:05:47 -0700

    Nueva Estructura

---
+++ Grupo_L-ejb/src/main/java/es/uma/informatica/Entidades/Asignatura.java
@@ -0,0 +1,266 @@
+package es.uma.informatica.Entidades;
+
+import java.io.Serializable;
+import java.lang.Long;
+import java.lang.String;
+import java.util.List;
+
+import javax.persistence.*;
+
+/**
+ * Entity implementation class for Entity: Asignatura
+ *
+ */
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+public class Asignatura implements Serializable {
+
+	   
+	@Id @GeneratedValue
+	private Long Referencia;
+	private Long Codigo;
+	private Long Créditos_Teoricos;
+	private Long Créditos_Practicos;
+	private Long Total_Créditos;
+	private String Ofertada;
+	private String Nombre;
+	private Long Curso;
+	private String Plazas;
+	private String Cuatrimestre;
+	private String Idioma_de_imparticion;
+	
+	public Asignatura(Long referencia, Long codigo, Long créditos_Teoricos, Long créditos_Practicos,
+			Long total_Créditos, String ofertada, String nombre, Long curso, String plazas, String cuatrimestre,
+			String idioma_de_imparticion) {
+		super();
+		Referencia = referencia;
+		Codigo = codigo;
+		Créditos_Teoricos = créditos_Teoricos;
+		Créditos_Practicos = créditos_Practicos;
+		Total_Créditos = total_Créditos;
+		Ofertada = ofertada;
+		Nombre = nombre;
+		Curso = curso;
+		Plazas = plazas;
+		Cuatrimestre = cuatrimestre;
+		Idioma_de_imparticion = idioma_de_imparticion;
+	}
+	
+	public Asignatura() {
+		super();
+	} 
+	
+	@ManyToOne
+	private Titulacion TA;
+	
+	@OneToMany(mappedBy = "AC")
+	private List<Clase> clases;
+	
+	@OneToMany(mappedBy = "asignatura")
+	private List<Asignaturas_Matrícula> asignaturasMatricula;
+	
+	@OneToMany(mappedBy = "A_GPA")
+	private List<GruposPorAsignatura> gruposPorAsignatura;
+	
+	@ManyToOne
+	private Matrícula matriculas;
+	
+	private static final long serialVersionUID = 1L;
+  
+	public Long getReferencia() {
+		return this.Referencia;
+	}
+
+	public void setReferencia(Long Referencia) {
+		this.Referencia = Referencia;
+	}   
+	public Long getCodigo() {
+		return this.Codigo;
+	}
+
+	public void setCodigo(Long Codigo) {
+		this.Codigo = Codigo;
+	}   
+	
+	public String getOfertada() {
+		return this.Ofertada;
+	}
+
+	public void setOfertada(String Ofertada) {
+		this.Ofertada = Ofertada;
+	}   
+	
+
+	public String getNombre() {
+		return Nombre;
+	}
+	public void setNombre(String nombre) {
+		Nombre = nombre;
+	}
+	  
+	public Long getCurso() {
+		return this.Curso;
+	}
+
+	public void setCurso(Long Curso) {
+		this.Curso = Curso;
+	}   
+	 
+	public String getIdioma_de_imparticion() {
+		return this.Idioma_de_imparticion;
+	}
+
+	public void setIdioma_de_imparticion(String Idioma_de_imparticion) {
+		this.Idioma_de_imparticion = Idioma_de_imparticion;
+	}	
+	
+	public Long getCréditos_Teoricos() {
+		return Créditos_Teoricos;
+	}
+	public void setCréditos_Teoricos(Long créditos_Teoricos) {
+		Créditos_Teoricos = créditos_Teoricos;
+	}
+	public Long getCréditos_Practicos() {
+		return Créditos_Practicos;
+	}
+	public void setCréditos_Practicos(Long créditos_Practicos) {
+		Créditos_Practicos = créditos_Practicos;
+	}
+	public Long getTotal_Créditos() {
+		return Total_Créditos;
+	}
+	public void setTotal_Créditos(Long total_Créditos) {
+		Total_Créditos = total_Créditos;
+	}
+	public String getPlazas() {
+		return Plazas;
+	}
+	public void setPlazas(String plazas) {
+		Plazas = plazas;
+	}
+	
+	public String getCuatrimestre() {
+		return Cuatrimestre;
+	}
+	public void setCuatrimestre(String cuatrimestre) {
+		Cuatrimestre = cuatrimestre;
+	}
+	public Titulacion getTA() {
+		return TA;
+	}
+	public void setTA(Titulacion tA) {
+		TA = tA;
+	}
+	public List<Clase> getClases() {
+		return clases;
+	}
+	public void setClases(List<Clase> clases) {
+		this.clases = clases;
+	}
+	public List<Asignaturas_Matrícula> getAsignaturasMatricula() {
+		return asignaturasMatricula;
+	}
+	public void setAsignaturasMatricula(List<Asignaturas_Matrícula> asignaturasMatricula) {
+		this.asignaturasMatricula = asignaturasMatricula;
+	}
+	
+	
+	public List<GruposPorAsignatura> getGruposPorAsignatura() {
+		return gruposPorAsignatura;
+	}
+	public void setGruposPorAsignatura(List<GruposPorAsignatura> gruposPorAsignatura) {
+		this.gruposPorAsignatura = gruposPorAsignatura;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((Codigo == null) ? 0 : Codigo.hashCode());
+		result = prime * result + ((Créditos_Practicos == null) ? 0 : Créditos_Practicos.hashCode());
+		result = prime * result + ((Créditos_Teoricos == null) ? 0 : Créditos_Teoricos.hashCode());
+		result = prime * result + ((Cuatrimestre == null) ? 0 : Cuatrimestre.hashCode());
+		result = prime * result + ((Curso == null) ? 0 : Curso.hashCode());
+		result = prime * result + ((Idioma_de_imparticion == null) ? 0 : Idioma_de_imparticion.hashCode());
+		result = prime * result + ((Nombre == null) ? 0 : Nombre.hashCode());
+		result = prime * result + ((Ofertada == null) ? 0 : Ofertada.hashCode());
+		result = prime * result + ((Plazas == null) ? 0 : Plazas.hashCode());
+		result = prime * result + ((Referencia == null) ? 0 : Referencia.hashCode());
+		result = prime * result + ((Total_Créditos == null) ? 0 : Total_Créditos.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Asignatura other = (Asignatura) obj;
+		if (Codigo == null) {
+			if (other.Codigo != null)
+				return false;
+		} else if (!Codigo.equals(other.Codigo))
+			return false;
+		if (Créditos_Practicos == null) {
+			if (other.Créditos_Practicos != null)
+				return false;
+		} else if (!Créditos_Practicos.equals(other.Créditos_Practicos))
+			return false;
+		if (Créditos_Teoricos == null) {
+			if (other.Créditos_Teoricos != null)
+				return false;
+		} else if (!Créditos_Teoricos.equals(other.Créditos_Teoricos))
+			return false;
+		if (Cuatrimestre == null) {
+			if (other.Cuatrimestre != null)
+				return false;
+		} else if (!Cuatrimestre.equals(other.Cuatrimestre))
+			return false;
+		if (Curso == null) {
+			if (other.Curso != null)
+				return false;
+		} else if (!Curso.equals(other.Curso))
+			return false;
+		if (Idioma_de_imparticion == null) {
+			if (other.Idioma_de_imparticion != null)
+				return false;
+		} else if (!Idioma_de_imparticion.equals(other.Idioma_de_imparticion))
+			return false;
+		if (Nombre == null) {
+			if (other.Nombre != null)
+				return false;
+		} else if (!Nombre.equals(other.Nombre))
+			return false;
+		if (Ofertada == null) {
+			if (other.Ofertada != null)
+				return false;
+		} else if (!Ofertada.equals(other.Ofertada))
+			return false;
+		if (Plazas == null) {
+			if (other.Plazas != null)
+				return false;
+		} else if (!Plazas.equals(other.Plazas))
+			return false;
+		if (Referencia == null) {
+			if (other.Referencia != null)
+				return false;
+		} else if (!Referencia.equals(other.Referencia))
+			return false;
+		if (Total_Créditos == null) {
+			if (other.Total_Créditos != null)
+				return false;
+		} else if (!Total_Créditos.equals(other.Total_Créditos))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Asignatura [Referencia=" + Referencia + ", Codigo=" + Codigo + ", Créditos_Teoricos="
+				+ Créditos_Teoricos + ", Créditos_Practicos=" + Créditos_Practicos + ", Total_Créditos="
+				+ Total_Créditos + ", Ofertada=" + Ofertada + ", Nombre=" + Nombre + ", Curso=" + Curso + ", Plazas="
+				+ Plazas + ", Cuatrimestre=" + Cuatrimestre + ", Idioma_de_imparticion=" + Idioma_de_imparticion
+				+ ", TA=" + TA + ", clases=" + clases + ", asignaturasMatricula=" + asignaturasMatricula
+				+ ", gruposPorAsignatura=" + gruposPorAsignatura + "]";
+	}
+}
---
+++ Grupo_L-ejb/src/test/java/es/uma/informatica/ejb/test/GrupoT.java
@@ -0,0 +1,189 @@
+package es.uma.informatica.ejb.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import es.uma.informatica.Entidades.Alumno;
+import es.uma.informatica.Entidades.Asignatura;
+import es.uma.informatica.Entidades.Asignaturas_Matrícula;
+import es.uma.informatica.Entidades.Expediente;
+import es.uma.informatica.Entidades.Grupo;
+import es.uma.informatica.Entidades.Matrícula;
+import es.uma.informatica.Entidades.Matrícula.MatriculaId;
+import es.uma.informatica.Entidades.Asignaturas_Matrícula.Asignaturas_MatriculaId;
+import es.uma.informatica.Exception.AlumnoException;
+import es.uma.informatica.Exception.GrupoException;
+import es.uma.informatica.Exception.GrupoExistenteException;
+import es.uma.informatica.Interfaces.InterfazAlumno;
+import es.uma.informatica.Interfaces.InterfazGrupo;
+import es.uma.informatica.sii.anotaciones.Requisitos;
+
+
+public class GrupoT {
+	private static final String Grupos_EJB = "java:global/classes/GrupoEJB";
+	private static final String Alumno_EJB = "java:global/classes/AlumnoEJB";
+	private static final String UNIDAD_PERSITENCIA_PRUEBAS = "SecretariaTest";
+	
+	private InterfazAlumno alumno;
+	private InterfazGrupo grupo;
+
+	@Before
+	public void setUp() throws Exception {
+		grupo = (InterfazGrupo) SuiteTest.ctx.lookup(Grupos_EJB);
+		alumno = (InterfazAlumno) SuiteTest.ctx.lookup(Alumno_EJB);
+		BaseDatos.inicializaBaseDatos(UNIDAD_PERSITENCIA_PRUEBAS);
+	}
+
+	@Test
+	@Requisitos({"RF5"})
+	public void testCrearGrupo(){
+		try {
+			Grupo g = new Grupo((long) 13, (long) 5, "F", "tarde", "no", (long) 5, (long) 6, (long) 30);
+			grupo.crearGrupo(g);
+			Grupo gr = grupo.leerGrupo(g.getID());
+			if((long)13 != gr.getID()) {
+				fail("No debería lanzarse excepción");
+			}
+		}catch(GrupoExistenteException e) {
+			fail("Los valores no son validos");
+		}catch(GrupoException e) {
+			fail("No debería lanzarse excepción");
+		}
+	}
+	
+	@Test
+	@Requisitos({"RF5"})
+	public void testLeerGrupo(){
+		try {
+            Grupo g = grupo.leerGrupo((long) 1);
+            assertEquals("A",g.getLetra());
+
+        }catch(GrupoException g) {
+            fail("No deberia lanzar una excepcion");
+        }
+	}
+	
+	@Test
+	@Requisitos({"RF5"})
+	public void testActualizarGrupo(){
+		try {
+			Grupo g1 = new Grupo ((long) 1, (long) 1, "A", "tarde", "si", (long) 8, (long) 75, (long) 80);
+            grupo.actualizarGrupo(g1);;
+            Grupo g2 = grupo.leerGrupo(g1.getID());
+            if(!g2.getID().equals(g1.getID())) {
+            	fail("Error al actualizar");
+            }
+        }catch(GrupoException g) {
+            fail("Lanza excepcion al actualizar");
+        }
+	}
+	
+	@Test
+	@Ignore
+	@Requisitos({"RF5"})
+	public void testEliminarGrupo() {
+		try {
+			Grupo gr = grupo.leerGrupo((long)78);
+			grupo.eliminarGrupo(gr);
+			
+			//MatriculaId m = new MatriculaId("Primero", (long) 33);
+			//Asignaturas_MatriculaId a = new Asignaturas_MatriculaId ((long) 1, m);
+			//Asignaturas_Matrícula gr3 = grupo.leerGrupoAsignatura(a);
+			assertEquals(null,gr.getID());
+			
+		} catch (GrupoException e) {
+			fail("No debería lanzarse excepción");
+		}
+	}
+	
+	
+	@Test
+	@Requisitos({"RF9"})
+	public void testSolicitarCambioGrupo() {
+		try {
+			Grupo A = grupo.leerGrupo((long)1);
+			Alumno Juan = alumno.leerAlumno((long)9);	
+			Asignatura Calculo = new Asignatura((long) 89, (long) 123, (long) 5, (long) 1, (long) 6, "Sí", "Cálculo", (long) 1, "50", "Primer Cuatrimestre", "Español");
+			grupo.solicitarCambioGrupo("Hola", A, Juan, Calculo);
+			
+		}catch(GrupoException e) {
+			fail("No debería lanzarse excepción");
+		} catch (AlumnoException e) {
+			fail("No debería lanzarse excepción");
+		}
+	}
+	
+	@Test
+	@Requisitos({"RF10"})
+	public void testSolicitarGrupo() {
+		try {
+			Grupo A = new Grupo((long) 1, (long) 1, "A", "tarde", "si", (long) 8, (long) 75, (long) 80);
+			Alumno Juan = alumno.leerAlumno((long)9);			
+			grupo.solicitarGrupo(A,Juan);
+			
+		}catch(GrupoException e) {
+			fail("No debería lanzarse excepción");
+		} catch (AlumnoException e) {
+			fail("No debería lanzarse excepción");
+		}
+	}
+	
+	@Test
+	@Requisitos({"RF11"})
+	public void testAsignarGrupo() {
+		try {
+			Alumno Juan = alumno.leerAlumno((long)9);
+			grupo.asignarGrupo(Juan);
+			Expediente e = Juan.getExpedientes().get(0);
+			Matrícula m = e.getMatriculas().get(0);
+			Asignaturas_Matrícula asigM = m.getAsigMatricula().get(0);
+			Grupo g = asigM.getG_AM();
+			assertNotEquals(g,null);
+		} catch (GrupoException e) {
+			fail("No debería lanzarse excepción");
+		} catch (AlumnoException e1) {
+			fail("No debería lanzarse excepción");
+		}
+		
+		/*try {
+			Grupo A = new Grupo((long) 1, (long) 1, "A", "tarde", "si", (long) 8, (long) 75, (long) 80);
+			Expediente e1 = new Expediente ( (long) 12, "activo", (float)8.75, (long) 120, (long) 60, (long) 60, (long) 0, (long) 0, (long) 0, (long)0);
+			Matrícula m1 = new Matrícula ("Primero", "activo", (long) 5, "tarde", new Date(14/03/2020), "si", La , e1);
+			
+			grupo.asignarGrupo(A, m1);
+			Grupo B = grupo.leerGrupo(A.getID());
+			List<Asignaturas_Matrícula> l1 = B.getAsignaturasMatriculas();
+			Matrícula m = matricula.leerMatricula(m1.getCurso_Academico());
+			List<Asignaturas_Matrícula> l2 = m.getAsigMatricula();
+			if(!l1.equals(l2)) {
+				fail("No debería lanzarse excepción");
+			}
+		}catch(GrupoException e) {
+			fail("No debería lanzarse excepción");
+		} catch (MatriculaException e) {
+			e.printStackTrace();
+		}*/
+	}
+	
+	@Test
+	@Requisitos({"RF21"})
+	public void testLeerAsignaturaMatricula(){
+		try {
+			//Expediente e1 = new Expediente ( (long) 12, "activo", (float)8.75, (long) 120, (long) 60, (long) 60, (long) 0, (long) 0, (long) 0, (long)0);
+			//Matrícula m1 = new Matrícula ("Primero", "activo", (long) 5, "tarde", new Date(14/03/2020), "si", "Cálculo, Matemáticas Discretas, Álgebra", e1);
+			//Asignatura Calculo = new Asignatura((long) 1, (long) 123, (long) 5, (long) 1, (long) 6, "Sí", "Cálculo", (long) 1, "50", "Primer Cuatrimestre", "Español");
+			MatriculaId m = new MatriculaId("Primero", (long) 33);
+			Asignaturas_MatriculaId a = new Asignaturas_MatriculaId ((long) 1, m);
+			Asignaturas_Matrícula am = grupo.leerGrupoAsignatura(a);
+            if(am==null) {
+            	fail("No deberia lanzar una excepcion");
+            }
+
+        }catch(GrupoException g) {
+            fail("No deberia lanzar una excepcion2");
+        }
+	}
+}
