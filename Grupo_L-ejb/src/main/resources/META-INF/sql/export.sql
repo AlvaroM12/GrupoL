a92992facefb653f68b0f12adeeba528182dd677
AlvaroM <alvaromm@uma.es>
2021-05-26 13:13:02 -0700

    2.0

--- Grupo_L-ejb/src/main/resources/META-INF/sql/export.sql
+++ Grupo_L-ejb/src/main/resources/META-INF/sql/export.sql
@@ -9,5 +9,7 @@ INSERT INTO Usuario (ID, Email_Institucional, Telefono, contrasenia) VALUES (1, 
 INSERT INTO Alumno (Apellido1, Apellido2, CP, DNI, Direccion, Email_Personal, Localidad, Movil, Nombre, Provincia, ID) VALUES ('Blanco', 'Zambrano', 46132, '57400152B', 'Reisedor 77', 'IsidreBlancoZambrano@dayrep.com', 'Almassera', 760544184, 'Isidre', 'MALAGA', 1);
 INSERT INTO Usuario (ID, Email_Institucional, Telefono, contrasenia) VALUES (2, 'rocio22@uma.es', 664184557, '123');
 INSERT INTO Alumno (Apellido1, Apellido2, CP, DNI, Direccion, Email_Personal, Localidad, Movil, Nombre, Provincia, ID) VALUES ('Molero', 'Marquez', 46132, '57400152A', 'Reisedor 77', 'IsidreBlancoZambrano@dayrep.com', 'Almassera', 760544184, 'Rocio', 'MALAGA', 2);
+INSERT INTO Usuario (ID, Email_Institucional, Telefono, contrasenia) VALUES (4, 'marta@uma.es', 664184557, '123');
+INSERT INTO Alumno (Apellido1, Apellido2, CP, DNI, Direccion, Email_Personal, Localidad, Movil, Nombre, Provincia, ID) VALUES ('Lopez', 'Perez', 46132, '57400152C', 'Reisedor 77', 'IsidreBlancoZambrano@dayrep.com', 'Almassera', 760544184, 'Marta', 'MALAGA', 4);
 INSERT INTO Usuario (ID, Email_Institucional, Telefono, contrasenia) VALUES (3, 'pepe@uma.es', 648152385, '123');
 INSERT INTO Personal_de_secretaria (Rango, ID) VALUES ('Jefe', 3);
