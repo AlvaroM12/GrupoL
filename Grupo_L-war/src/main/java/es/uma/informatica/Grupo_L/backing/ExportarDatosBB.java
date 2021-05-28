9feb3c6a7dda7996446b9a77c238691b3f2a1028
Javier Garcia <javierganto@uma.es>
2021-05-28 10:20:50 -0700

    Exportar

--- Grupo_L-war/src/main/java/es/uma/informatica/Grupo_L/backing/ExportarDatosBB.java
+++ Grupo_L-war/src/main/java/es/uma/informatica/Grupo_L/backing/ExportarDatosBB.java
@@ -2,10 +2,17 @@ package es.uma.informatica.Grupo_L.backing;
 
 
 
+import java.io.IOException;
+
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+
+import es.uma.informatica.Entidades.Titulacion;
+import es.uma.informatica.Exception.DatosException;
+import es.uma.informatica.Exception.TitulacionException;
 import es.uma.informatica.Interfaces.InterfazDatos;
+import es.uma.informatica.Interfaces.InterfazTitulacion;
 
 @Named(value = "exportar")
 @RequestScoped
@@ -13,9 +20,24 @@ public class ExportarDatosBB {
 	
     @Inject
     private InterfazDatos datosejb;
+    @Inject
+    private InterfazTitulacion titulacion;
+    private String selectedOption;
     
+    public String getSelectedOption() {
+        return selectedOption;
+    }
+
+    public void setSelectedOption(String selectedOption) {
+        //this.selectedOption = selectedOption;
+    }
     
-    
+    public void exDatos() throws DatosException, IOException, TitulacionException {
+    	long codigo=Long.parseLong(selectedOption);
+    	Titulacion t=titulacion.consultarTitulacion(codigo);
+    	
+    	datosejb.exportarDatos(t);
+    }
     
     
     
