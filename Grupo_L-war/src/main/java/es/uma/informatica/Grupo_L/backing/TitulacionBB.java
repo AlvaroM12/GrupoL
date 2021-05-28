325a77ead5238c21e9bd565485e6ba411eb5ec4a
Javier Garcia <javierganto@uma.es>
2021-05-28 05:41:44 -0700

    titulabb

--- Grupo_L-war/src/main/java/es/uma/informatica/Grupo_L/backing/TitulacionBB.java
+++ Grupo_L-war/src/main/java/es/uma/informatica/Grupo_L/backing/TitulacionBB.java
@@ -1,11 +1,13 @@
 package es.uma.informatica.Grupo_L.backing;
 
 
+import java.util.ArrayList;
 import java.util.List;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 import es.uma.informatica.Entidades.Centro;
+import es.uma.informatica.Entidades.Expediente;
 import es.uma.informatica.Entidades.Titulacion;
 import es.uma.informatica.Exception.TitulacionException;
 import es.uma.informatica.Interfaces.InterfazTitulacion;
@@ -13,7 +15,8 @@ import es.uma.informatica.Interfaces.InterfazTitulacion;
 @Named(value = "titulacion")
 @RequestScoped
 public class TitulacionBB {
-
+	@Inject
+	private InfoSesion infosesion;
     @Inject
     private InterfazTitulacion titulacion;
     
@@ -27,6 +30,18 @@ public class TitulacionBB {
     
     public Titulacion gettit() {
     	return tit;
+    }
+    
+    
+    public List<Titulacion> getTitulacion()
+    {
+		List<Titulacion> titula = new ArrayList<Titulacion>();
+        for (Expediente e : infosesion.getAlumno().getExpedientes()) {
+			
+				titula.add(e.getTE());	
+				
+		}
+        return titula;
     }
     
     public  synchronized List<Titulacion> getTitulaciones(){
