164da10d1f34c531a3c4a532d0710264c8efadcf
AlvaroM <alvaromm@uma.es>
2021-06-04 03:47:40 -0700

    Descargar arreglado

--- Grupo_L-war/src/main/java/es/uma/informatica/Grupo_L/backing/FileDownloadView.java
+++ Grupo_L-war/src/main/java/es/uma/informatica/Grupo_L/backing/FileDownloadView.java
@@ -1,8 +1,11 @@
 package es.uma.informatica.Grupo_L.backing;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
+import java.util.logging.Logger;
 
 import javax.enterprise.context.RequestScoped;
 import javax.faces.context.FacesContext;
@@ -11,6 +14,8 @@ import javax.inject.Named;
 
 import org.primefaces.model.DefaultStreamedContent;
 import org.primefaces.model.StreamedContent;
+
+import es.uma.informatica.EJB.MatriculaEJB;
 @Named
 @RequestScoped
 public class FileDownloadView {
@@ -19,15 +24,23 @@ public class FileDownloadView {
 	
     private StreamedContent file;
     
+    private final static Logger LOGGER=Logger.getLogger(FileDownloadView.class.getCanonicalName());
     public FileDownloadView() {
-    	
-    	
-		
+    	LOGGER.info("Hola");
         file = DefaultStreamedContent.builder()
                 .name("Informatica.xls")
                 .contentType("application/vnd.ms-excel")
-                .stream(() -> FacesContext.getCurrentInstance().getExternalContext().getResourceAsStream("/opt/jboss/wildfly/docs/"+expD.getName()+".xls"))
+                .stream(() -> {
+					try {
+						return new FileInputStream("/opt/jboss/wildfly/docs/Informatica.xls");
+					} catch (FileNotFoundException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					return null;
+				})
                 .build();
+        LOGGER.info("Adios");
     }
 
     public StreamedContent getFile() {
